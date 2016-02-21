d63c51c98c5a8cc0d2d1c06768e94485ad37844d
Ivan Cisneros <icisner@gmail.com>
2016-02-21 11:01:37 -0600

    cats clicker ver1

---
+++ js/cats.js
@@ -0,0 +1,9 @@
+function addCatCount(){
+	nCat=nCat+1;
+	document.getElementById("Cat1").innerHTML =nCat ;
+}
+
+function resetCatCount(){
+	nCat=0;
+	document.getElementById("Cat1").innerHTML =nCat ;
+}
---
+++ js/main.js
@@ -0,0 +1,19 @@
+function initCanvas(){
+    var ctx = document.getElementById('my_canvas').getContext('2d');
+    ctx.canvas.addEventListener('mousemove', function(event){
+        var mouseX = event.clientX - ctx.canvas.offsetLeft;
+        var mouseY = event.clientY - ctx.canvas.offsetTop;
+        var status = document.getElementById('status');
+        status.innerHTML = mouseX+" | "+mouseY;
+    });
+    ctx.canvas.addEventListener('click', function(event){
+        var mouseX = event.clientX - ctx.canvas.offsetLeft;
+        var mouseY = event.clientY - ctx.canvas.offsetTop;
+        // alert(mouseX+" | "+mouseY);
+        ctx.fillStyle = "orange";
+        ctx.fillRect(mouseX-15, mouseY-15, 30, 30);
+    });
+}
+window.addEventListener('load', function(event) {
+    initCanvas();
+});
